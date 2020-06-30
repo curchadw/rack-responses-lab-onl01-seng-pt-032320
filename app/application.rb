f3499fe172a952bf971990d4a6689e5cf1b7966f
Curtis Chadwell <laurencechadwell@gmail.com>
2020-06-30 03:11:34 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    
+    time = Time.now
+    if time.hour <= 12 
+      resp.write "#{time}\n"
+      resp.write "Good Morning!"
+    else
+      resp.write "#{time}\n"
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
